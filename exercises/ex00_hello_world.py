504b03f4e5fb1131dc4b8e6a681d65e120d45e51
Heidi Alkoutami <heidialk@ad.unc.edu>
2022-01-17 16:52:49 -0500

    "Finished Exercise 00!"

---
+++ exercises/ex00_hello_world.py
@@ -0,0 +1,5 @@
+"""My first program for COMP110."""
+__author__ = "730412587"
+
+
+print("Hello, world. is tradition") 
---
+++ lessons/ls4.py
@@ -0,0 +1,19 @@
+from random import random
+
+
+chr(0b01100001)
+type(True)
+1_000+1_000_000
+10 / 2
+"U" + "N" + "C"
+"12345"[0]
+"spooky season"[2+2]
+"spooky"[len("spooky")]
+x: int = 1
+x = x + 1
+x
+course: str = "110"
+course: str = "110"
+type(int(course))
+from random inport random
+type(random)
